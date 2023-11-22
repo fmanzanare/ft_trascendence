6845f13880311295c541791525e1eecc5034e340
Léo <leogaudinoff@gmail.com>
2023-11-22 16:51:20 +0100

    fix: fix non-required fields

--- django/pongue/models.py
+++ django/pongue/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import AbstractUser
 class PongueUser(AbstractUser):
 	id = models.AutoField(primary_key=True)
 	username = models.CharField(max_length=50, unique=True)
-	access_token = models.CharField(max_length=50)
+	access_token = models.CharField(max_length=50, blank=True)
 	display_name = models.CharField(max_length=50)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
-	avatar_base64 = models.TextField(default="")
+	avatar_base64 = models.TextField(default="", blank=True)
 	status = models.CharField(max_length=50, default="offline")
 	games_won = models.IntegerField(default=0)
 	games_lost = models.IntegerField(default=0)
