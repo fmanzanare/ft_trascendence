0a9acd198a9301014aa8320a12c04b10817a2090
Léo <leogaudinoff@gmail.com>
2023-11-22 16:52:17 +0100

    feat: add 2FA

--- django/pongue/urls.py
+++ django/pongue/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path("login/", views.login, name="login"),
 	path("logout/", views.logout, name="logout"),
 	path("auth/", views.auth, name="auth"),
+	path("submit2fa/", views.submit2fa, name="submit2fa"),
 ]
--- django/pongue/views.py
+++ django/pongue/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 import requests
 import os
 from .models import PongueUser
+from .otp import totp
+import base64, hashlib
 
 @login_required(login_url="login")
 def index(request):
@@ -36,21 +38,44 @@ def login(request):
 		user = authenticate(request, username=username, password=password)
 
 		if user is not None:
-			auth_login(request, user)
-			return redirect("index")
+			return pass2fa(request, user)
 		else:
 			messages.info(request, "Username or password is incorrect")
 
 	return render(request, "login.html")
+
+def pass2fa(request, user_obj):
+	if user_obj.has_2fa:
+		hashed_secret = hashlib.sha512((user_obj.username + os.environ.get("OTP_SECRET")).encode("utf-8")).digest()
+		encoded_secret = base64.b32encode(hashed_secret)
+		return render(request, "pass2fa.html", {"user": user_obj.username,"key": encoded_secret})
+	else:
+		auth_login(request, user_obj)
+		return redirect("index")
+
+def submit2fa(request):
+	if request.method == "POST":
+		user = PongueUser.objects.get(username=request.POST.get("user"))
+		hashed_secret = hashlib.sha512((user.username + os.environ.get("OTP_SECRET")).encode("utf-8")).digest()
+		encoded_secret = base64.b32encode(hashed_secret)
+		if totp(encoded_secret) == request.POST.get("code"):
+			auth_login(request, user)
+			return redirect("index")
+		else:
+			messages.info(request, "Invalid OTP")
+			return redirect("login")
+	else:
+		messages.info(request, "Invalid method")
+		return redirect("login")
 
 @login_required(login_url="login")
 def logout(request):
 	auth_logout(request)
 	return redirect("login")
 
-
-
 def auth(request):
+	if request.user.is_authenticated:
+		return redirect("index")
 	if request.method == "GET":
 		code = request.GET.get("code")
 		if code:
@@ -69,13 +94,13 @@ def auth(request):
 
 			try:
 				user = PongueUser.objects.get(username=username)
-				auth_login(request, user)
+				return pass2fa(request, user)
 			except PongueUser.DoesNotExist:
 				user = PongueUser.objects.create_user(username=username, display_name=display_name)
 				auth_login(request, user)
-
-			return redirect("index")
 		else:
-			return render(request, "auth.html", {"error": "Invalid authorization code"})
+			messages.info(request, "Invalid authorization code")
+			return redirect("login")
 	else:
-		return render(request, "auth.html", {"error": "Invalid request method"})
+		messages.info(request, "Invalid method")
+		return redirect("login")
