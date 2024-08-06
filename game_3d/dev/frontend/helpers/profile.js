01297ea5225ac8044b8a25b6e0088d9e3a7fb0df
AntonioJesusRM <Antjrm95@gmail.com>
2024-08-06 21:44:21 +0200

    put offline when close nav

--- game_3d/dev/frontend/helpers/profile.js
+++ game_3d/dev/frontend/helpers/profile.js
@@ -1,32 +1,5 @@
 import {twoFactorChecked, twoFactorUnchecked, conectServerChange} from "./changeDataUserUtils.js"
 
-export function logOut()
-{
-	const $token = sessionStorage.getItem('pongToken');
-	const $logoutUrl = apiUrl + 'logout/';
-	fetch($logoutUrl, {
-		method: 'GET',
-		headers: {
-			"Authorization": $token
-		}
-	})
-	.then(response => {
-		if (!response.ok) {
-			throw new Error(`Error en la solicitud: ${response.status}`);
-		}
-		return response.json()
-	})
-	.then(data => {
-		console.log(data);
-		sessionStorage.clear();
-		window.history.pushState(null, null, '/');
-		window.location.reload();
-	})
-	.catch(error => {
-		console.error('Error en la solicitud:', error);
-	});
-}
-
 export function generateQr()
 {
 	const $token = sessionStorage.getItem('pongToken');
