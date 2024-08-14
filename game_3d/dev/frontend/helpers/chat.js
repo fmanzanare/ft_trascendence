6feb52c7e54aee6334684065b3fb14cee3032af4
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2024-08-14 14:45:36 +0200

    game invitation working

--- game_3d/dev/frontend/helpers/chat.js
+++ game_3d/dev/frontend/helpers/chat.js
@@ -1,4 +1,5 @@
 import { openChatWebSockets } from "../index.js";
+import { acceptGameInvitation } from "./gameMode.js";
 
 /**
  * Shows the current chat friend's name in the upper chat bar and clears the chat log.
@@ -22,7 +23,7 @@ function showCurrentChatFriendName(friendName, friendshipId) {
 
 	// Create a button element
 	const playButton = document.createElement('button');
-	playButton.innerText = 'Jugar';
+	playButton.innerText = 'Play';
 
 	// Set the button's style
 	playButton.setAttribute("class", "start-0 position-absolute");
@@ -37,10 +38,37 @@ function showCurrentChatFriendName(friendName, friendshipId) {
 }
 
 function gameInvitation() {
-	console.log("openChatWebSockets:", openChatWebSockets);
 	const friendshipId = this.getAttribute("data-friendship-id");
+	console.log("friendshipId game invitation:", friendshipId);
+	const chatSocket = openChatWebSockets[friendshipId].chatSocket;
+
+	
 	if (openChatWebSockets[friendshipId].gameInvitationReceived) {
-		console.log("gameInvitationReceived");
+		console.log("Game invitation accepted");
+		if (chatSocket && chatSocket.readyState === WebSocket.OPEN) {
+			chatSocket.send(JSON.stringify({
+				message: "",
+				chatId: friendshipId,
+				senderId: sessionStorage.getItem('userId'),
+				gameInvitation: false,
+				gameInvitationResponse: true
+		}))};
+
+		openChatWebSockets[friendshipId].gameInvitationReceived = false;
+		const hostGameId = openChatWebSockets[friendshipId].hostGameId;
+		const guestGameId = openChatWebSockets[friendshipId].guestGameId;
+
+		console.log("hostGameId:", hostGameId, "guestGameId:", guestGameId);
+		// Remove the 'Reject' button from the upperChatBar
+		const rejectButton = document.querySelector('#upper-bar button.rejectButton[data-friendship-id]');
+		if (rejectButton) {
+			rejectButton.remove();
+		}
+		acceptGameInvitation(hostGameId, guestGameId);
+		openChatWebSockets[friendshipId].gameInvitationReceived = false;
+		openChatWebSockets[friendshipId].gameInvitationResponse = false;
+		openChatWebSockets[friendshipId].hostGameId = null;
+		openChatWebSockets[friendshipId].guestGameId = null;
 		return;
 	} else {
 		const userId = sessionStorage.getItem('userId');
@@ -51,8 +79,11 @@ function gameInvitation() {
 				message: "",
 				chatId: friendshipId,
 				senderId: userId,
-				gameInvitation: true
+				gameInvitation: true,
+				gameInvitationResponse: false,
+				hostGameId: userId,
 			}));
+			openChatWebSockets[friendshipId].hostGameId = userId;
 			messageInputDom.value = ''; // Clean input before send
 			document.querySelector('#chat-log').value += "Game invitation sent\n";
 		}
@@ -60,6 +91,18 @@ function gameInvitation() {
 	}
 }
 
+function rejectGameInvitation() {
+	const friendshipId = this.getAttribute("data-friendship-id");
+	const chatSocket = openChatWebSockets[friendshipId].chatSocket;
+	if (chatSocket && chatSocket.readyState === WebSocket.OPEN) {
+		chatSocket.send(JSON.stringify({
+			message: "Game invitation rejected",
+			chatId: friendshipId,
+			senderId: sessionStorage.getItem('userId'),
+			gameInvitationResponse: false
+		}));
+	}
+}
 // Retrieves the list of friends from the server and prints them.
 function getChatMessages(friendshipId) {
 
@@ -130,21 +173,61 @@ export function handleChatInput(friendship, friendName) {
 		openChatWebSockets[friendship.friendshipId].chatSocket = chatSocket;
 		openChatWebSockets[friendship.friendshipId].chatMessages = [];
 		openChatWebSockets[friendship.friendshipId].gameInvitationReceived = false;
+		openChatWebSockets[friendship.friendshipId].gameInvitationResponse = false;
+		openChatWebSockets[friendship.friendshipId].hostGameId = null;
+		openChatWebSockets[friendship.friendshipId].guestGameId = null;
 		openChatWebSockets[friendship.friendshipId].chatNotification = false;
 		
 		// Assign event handlers here to ensure they are applied to the new WebSocket
 		openChatWebSockets[friendship.friendshipId].chatSocket.onmessage = function (e) {
 			const data = JSON.parse(e.data);
+			console.log('Received message:', data);
 			if (data.senderUsername === document.querySelector('#friendNameUpperBar').getAttribute('data-username')
 				&& data.message.trim() !== '') {
 				document.querySelector('#chat-log').value += data.message;
 			}
-			if (data.message.trim() !== '') {
+			if (data.message.trim() !== '' && data.gameInvitation === false) {
 				openChatWebSockets[friendship.friendshipId].chatMessages.push({
 					senderId: sessionStorage.getItem('userId'),
 					isRead: false,
-					message: data.message
-					});
+					message: data.message,
+					chatId: friendship.friendshipId,
+				});
+			} // the user receives a game invitation
+			else if (data.gameInvitation === true && data.senderUsername !== sessionStorage.getItem('userName')) {
+				openChatWebSockets[friendship.friendshipId].gameInvitationReceived = true;
+				openChatWebSockets[friendship.friendshipId].hostGameId = data.senderId;
+				openChatWebSockets[friendship.friendshipId].guestGameId = sessionStorage.getItem('userId');
+
+				document.querySelector('#chat-log').value += 'Game invitation received. Click on "Play" button, or "Reject" button\n';
+				let upperChatBar = document.getElementById('upper-bar');
+				// Create a button element for rejecting the game invitation
+				const rejectButton = document.createElement('button');
+				rejectButton.innerText = 'Reject';
+
+				// Set the button's style
+				rejectButton.setAttribute("class", "rejectButton start-0 position-absolute");
+				rejectButton.setAttribute("style", "margin-left: 55px;");
+				rejectButton.setAttribute("data-friendship-id", friendship.friendshipId);
+				rejectButton.onclick = rejectGameInvitation;
+
+				// Append the button to the upperChatBar
+				upperChatBar.appendChild(rejectButton);
+			} else if (data.gameInvitationResponse === true && data.senderUsername !== sessionStorage.getItem('userName')) {
+				openChatWebSockets[friendship.friendshipId].gameInvitationReceived = false;
+				openChatWebSockets[friendship.friendshipId].gameInvitationResponse = false;
+				openChatWebSockets[friendship.friendshipId].hostGameId = null;
+				openChatWebSockets[friendship.friendshipId].guestGameId = null;
+				
+				console.log("hostGameId:", sessionStorage.getItem('userId'), "guestGameId:", data.senderId);
+				document.querySelector('#chat-log').value += 'Game invitation accepted. Starting Game\n';
+				acceptGameInvitation(sessionStorage.getItem('userId'), data.senderId);
+			} else if (data.gameInvitationResponse === false && data.senderUsername !== sessionStorage.getItem('userName')) {
+				openChatWebSockets[friendship.friendshipId].gameInvitationReceived = false;
+				openChatWebSockets[friendship.friendshipId].gameInvitationResponse = false;
+				openChatWebSockets[friendship.friendshipId].hostGameId = null;
+				openChatWebSockets[friendship.friendshipId].guestGameId = null;
+				document.querySelector('#chat-log').value += 'Game invitation rejected\n';
 			}
 		};
 
